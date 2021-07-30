8fe784c5d71c236cdf7a5382acddd064b849bae5
LordGhostX <solomonghost9@gmail.com>
2021-07-30 17:52:32 +0100

    adusted params

--- solution.py
+++ solution.py
@@ -1,13 +1,13 @@
-def solution(args):
+def solution(numbers, target):
     # generate a map of the numbers in the input
     # the values become the keys and the index become the values
-    number_map = {args[0][i]: i for i in range(len(args[0]))}
+    number_map = {numbers[i]: i for i in range(len(numbers))}
 
     # iterate through the keys in the number map
     for k in number_map:
         # to find a sum, check if the half sum is also a key in the map
         # runtime ~ O(n), where n = the length of the input array
-        half_num = args[1] - k
+        half_num = target - k
         if number_map.get(half_num):
             first_index = number_map[k]
             second_index = number_map[half_num]
