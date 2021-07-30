00d66b80f5c4d4b62197e7b62369238ae2b5b303
LordGhostX <solomonghost9@gmail.com>
2021-07-30 17:40:58 +0100

    integrated itertools

--- solution.py
+++ solution.py
@@ -1,5 +1,9 @@
+from itertools import combinations
+
+
 def solution(args):
-    for i, j in enumerate(args[0]):
-        for k, l in enumerate(args[0]):
-            if j + l == args[1]:
-                return i, k
+    for number_set in combinations(args[0], 2):
+        if sum(number_set) == args[1]:
+            first_index = args[0].index(number_set[0])
+            second_index = args[0].index(number_set[1])
+            return first_index, second_index
