72de857c03cd4776699c0ceb27d09054aef47325
LordGhostX <solomonghost9@gmail.com>
2021-07-30 17:32:56 +0100

    fixed code

--- solution.py
+++ solution.py
@@ -2,4 +2,4 @@ def solution(array_of_integers, integer):
     for i, j in enumerate(array_of_integers):
         for k, l in enumerate(array_of_integers):
             if j + l == integer:
-                return i, j
+                return i, k
